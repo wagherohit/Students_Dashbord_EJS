4f804f2385b07065fbfa822fe3bc052b707dd700
wagherohit <rohitwaghe45@gmail.com>
2025-09-27 16:25:38 +0530

    Student Dashboard Backend Assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.render('pages/home.ejs')
+})
+
+app.get('/students',(req,res)=>{
+    const students = [
+  { name: "Aarav Mehta", rollNo: "101", course: "ExpressJS", batch: "10-12 AM" },
+  { name: "Riya Sharma", rollNo: "102", course: "MongoDB", batch: "12-3 PM" },
+  { name: "Rohit Patil", rollNo: "103", course: "NodeJS", batch: "2:30-4:30 PM" },
+  { name: "Neha Kulkarni", rollNo: "104", course: "EJS", batch: "5-6 PM" }
+];
+
+   res.render('pages/students.ejs', {students})
+})
+
+app.get('/contact',(req,res)=>{
+   res.render('pages/contact.ejs')
+})
+
+const PORT =3000
+
+app.listen(PORT,()=>{
+    console.log(`server is running ${PORT}`)
+})
